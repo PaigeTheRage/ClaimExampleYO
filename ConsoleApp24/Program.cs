2ab88816d76631bfad094f0a8c422bdafe3954a5
PaigeTheRaige <paigeleighparsons@gmail.com>
2017-08-03 16:01:24 -0700

    initial commit

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the height of your triangle?");
+            double height = Convert.ToDouble(Console.ReadLine());
+            double width = Convert.ToDouble(Console.ReadLine());
+            double area= Convert.ToDouble(.5 * width * height);
+            Console.WriteLine("The area of your triangle is: " + area);
+            Console.ReadLine();
+        }
+    }
+    }
+
